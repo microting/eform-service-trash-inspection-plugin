efae6afe39e4945bed40b30d5f94bb2c101db5c7
René Schultz Madsen <rm@microting.dk>
2024-05-10 06:21:06 +0200

    Updating to .net 8.

--- ServiceTrashInspectionPlugin/Installers/RebusInstaller.cs
+++ ServiceTrashInspectionPlugin/Installers/RebusInstaller.cs
@@ -23,34 +23,49 @@ using Castle.MicroKernel.Registration;
 using Castle.MicroKernel.SubSystems.Configuration;
 using Castle.Windsor;
 using Rebus.Config;
+using Rebus.Logging;
 
 namespace ServiceTrashInspectionPlugin.Installers
 {
     public class RebusInstaller : IWindsorInstaller
     {
-        private readonly string connectionString;
-        private readonly int maxParallelism;
-        private readonly int numberOfWorkers;
+        private readonly string _connectionString;
+        private readonly int _maxParallelism;
+        private readonly int _numberOfWorkers;
+        private readonly string _rabbitMqUser;
+        private readonly string _rabbitMqPassword;
+        private readonly string _rabbitMqHost;
+        private readonly string _customerNo;
 
-        public RebusInstaller(string connectionString, int maxParallelism, int numberOfWorkers)
+        public RebusInstaller(string customerNo, string connectionString, int maxParallelism, int numberOfWorkers, string rabbitMqUser, string rabbitMqPassword, string rabbitMqHost)
         {
-            if (string.IsNullOrEmpty(connectionString)) throw new ArgumentNullException(nameof(connectionString));
-            this.connectionString = connectionString;
-            this.maxParallelism = maxParallelism;
-            this.numberOfWorkers = numberOfWorkers;
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new ArgumentNullException(nameof(connectionString));
+            }
+            _connectionString = connectionString;
+            _maxParallelism = maxParallelism;
+            _numberOfWorkers = numberOfWorkers;
+            _rabbitMqUser = rabbitMqUser;
+            _rabbitMqPassword = rabbitMqPassword;
+            _rabbitMqHost = rabbitMqHost;
+            _customerNo = customerNo;
         }
 
         public void Install(IWindsorContainer container, IConfigurationStore store)
         {
+            Console.WriteLine($"rabbitMqUser: {_rabbitMqUser}");
+            Console.WriteLine($"rabbitMqPassword: {_rabbitMqPassword}");
+            Console.WriteLine($"rabbitMqHost: {_rabbitMqHost}");
             Configure.With(new CastleWindsorContainerAdapter(container))
-                .Logging(l => l.ColoredConsole())
-                .Transport(t => t.UseRabbitMq("amqp://admin:password@localhost", "eform-service-trashinspection-plugin"))
+                .Logging(l => l.ColoredConsole(LogLevel.Info))
+                .Transport(t => t.UseRabbitMq($"amqp://{_rabbitMqUser}:{_rabbitMqPassword}@{_rabbitMqHost}", $"{_customerNo}-eform-service-trash-inspection-plugin"))
                 .Options(o =>
                 {
-                    o.SetMaxParallelism(maxParallelism);
-                    o.SetNumberOfWorkers(numberOfWorkers);
+                    o.SetMaxParallelism(_maxParallelism);
+                    o.SetNumberOfWorkers(_numberOfWorkers);
                 })
-                .Start();            
+                .Start();
         }
     }
 }
